b7f2157565e3c7555daee7d43b1ffe2c89bff91d
Lena <elk230827@yandex.ru>
2025-12-29 17:25:22 +0300

    parametrized test

--- pages/home_page.py
+++ pages/home_page.py
@@ -17,68 +17,21 @@ class HomePage(SamokatPage):
     header = (By.XPATH, '//div[contains( @class, "Home_Header")]' )
 
 
-    question_cost = (By.XPATH, f'//div[contains(text(), "{QA[0][1]}")]' )
-    question_number = (By.XPATH, f'//div[contains(text(), "{QA[1][1]}")]' )
-    question_time = (By.XPATH, f'//div[contains(text(), "{QA[2][1]}")]' )
-    question_today = (By.XPATH, f'//div[contains(text(), "{QA[3][1]}")]' )
-    question_expand = (By.XPATH, f'//div[contains(text(), "{QA[4][1]}")]' )
-    question_charge = (By.XPATH, f'//div[contains(text(), "{QA[5][1]}")]' )
-    question_cancel = (By.XPATH, f'//div[contains(text(), "{QA[6][1]}")]' )
-    question_mkad = (By.XPATH, f'//div[contains(text(), "{QA[7][1]}")]' )
-
-    ans_cost = (By.XPATH, f'//p[contains(text(), "{QA[0][2]}")]' )
-    ans_number = (By.XPATH, f'//p[contains(text(), "{QA[1][2]}")]' )
-    ans_time = (By.XPATH, f'//p[contains(text(), "{QA[2][2]}")]' )
-    ans_today = (By.XPATH, f'//p[contains(text(), "{QA[3][2]}")]' )
-    ans_expand = (By.XPATH, f'//p[contains(text(), "{QA[4][2]}")]' )
-    ans_charge = (By.XPATH, f'//p[contains(text(), "{QA[5][2]}")]' )
-    ans_cancel = (By.XPATH, f'//p[contains(text(), "{QA[6][2]}")]' )
-    ans_mkad = (By.XPATH, f'//p[contains(text(), "{QA[7][2]}")]' )
-
-
-
-    @allure.step(f"Проверка вопроса {QA[0][1]}")
-    def check_question_cost(self):
-        return self.check_question(self.question_cost, self.ans_cost)
-
-    @allure.step(f"Проверка вопроса {QA[1][1]}")
-    def check_question_number(self):
-        return self.check_question(self.question_number, self.ans_number)
-
-    @allure.step(f"Проверка вопроса {QA[2][1]}")
-    def check_question_time(self):
-        return self.check_question(self.question_time, self.ans_time)
-
-    @allure.step(f"Проверка вопроса {QA[3][1]}")
-    def check_question_today(self):
-        return self.check_question(self.question_today, self.ans_today)
-
-    @allure.step(f"Проверка вопроса {QA[4][1]}")
-    def check_question_expand(self):
-        return self.check_question(self.question_expand, self.ans_expand)
-
-    @allure.step(f"Проверка вопроса {QA[5][1]}")
-    def check_question_charge(self):
-        return self.check_question(self.question_charge, self.ans_charge)
-
-    @allure.step(f"Проверка вопроса {QA[6][1]}")
-    def check_question_cancel(self):
-        return self.check_question(self.question_cancel, self.ans_cancel)
-
-    @allure.step(f"Проверка вопроса {QA[7][1]}")
-    def check_question_mkad(self):
-        return self.check_question(self.question_mkad, self.ans_mkad)
-
+    
+    @allure.step("Проверка вопроса q")
     def check_question(self, q, ans):
-        qs = self.find(*q)
+
+        q_locator = f'//div[contains( text(), "{q}")]'
+        ans_locator = f'//p[contains( text(), "{ans}")]'
+        qs = self.find(By.XPATH, q_locator)
         
         self.scroll_to_elment(qs)
 
         qs.click()
 
-        self.wait(ans)
+        self.wait( (By.XPATH, ans_locator) )
 
-        ans_el = self.find(*ans)
+        ans_el = self.find(By.XPATH, ans_locator)
         return ans_el.is_displayed()
 
 
--- tests/test_questions.py
+++ tests/test_questions.py
@@ -13,34 +13,8 @@ class TestQuestions(BaseTest):
     def test_question_header(self, home_page):
         assert home_page.check_header()
 
+    @pytest.mark.parametrize("idx,q,a", QA )
     @allure.title(f"Проверка вопрос {QA[0][1]}")
-    def test_question_cost(self, home_page):
-        assert home_page.check_question_cost()
+    def test_question(self, home_page, idx, q, a):
+        assert home_page.check_question(q,a)
 
-    @allure.title(f"Проверка вопрос {QA[1][1]}")
-    def test_question_number(self, home_page):
-        assert home_page.check_question_number()
-
-    @allure.title(f"Проверка вопрос {QA[2][1]}")
-    def test_question_time(self, home_page):
-        assert home_page.check_question_time()
-
-    @allure.title(f"Проверка вопрос {QA[3][1]}")
-    def test_question_today(self, home_page):
-        assert home_page.check_question_today()
-
-    @allure.title(f"Проверка вопрос {QA[4][1]}")
-    def test_question_expand(self, home_page):
-        assert home_page.check_question_expand()
-
-    @allure.title(f"Проверка вопрос {QA[5][1]}")
-    def test_charge(self, home_page):
-        assert home_page.check_question_charge()
-
-    @allure.title(f"Проверка вопрос {QA[6][1]}")
-    def test_question_cancel(self, home_page):
-        assert home_page.check_question_cancel()
-
-    @allure.title(f"Проверка вопрос {QA[7][1]}")
-    def test_question_mkad(self, home_page):
-        assert home_page.check_question_mkad()
